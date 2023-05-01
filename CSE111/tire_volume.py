5f9f17839acf13304d4fb7ade2635535b405ce7b
maureendeguzman <yhumi30@gmail.com>
2023-05-01 22:12:21 +0800

    added thr home for wdd130

--- CSE111/tire_volume.py
+++ CSE111/tire_volume.py
@@ -11,7 +11,6 @@ text2 = (input("Enter the aspect ratio of the tire (ex 60): "))
 aspect_ratio = float(text2)
 text3 = (input("Enter the diameter of the wheel in inches (ex 15): "))
 diameter = float(text3)
-
 # calculate tire volume in liters
 pi = 3.14159
 height = (width * aspect_ratio / 100) / 2.80
@@ -20,12 +19,14 @@ volume = pi * (height ** 2) * (diameter / 2) * 0.001
 # Open the file for appending text
 with open("volumes.txt", "a") as file:
     # Write the values to the file
-    file.write(f"Width: {width}\n")
-    file.write(f"Aspect Ratio: {aspect_ratio}\n")
-    file.write(f"Diameter: {diameter}\n")
-    file.write(f"Volume: {volume}\n")
+    file.write(f"Width: {width}")
+    file.write(f"Aspect Ratio: {aspect_ratio}")
+    file.write(f"Diameter: {diameter}")
+    file.write(f"Volume: {volume}")
+print()
+# print the approximate tire volume
+print(f"The approximate volume is {volume:.2f} liters")
 print()
 print(f"{current_date_and_time:%Y-%m-%d}, {width:.2f}, {aspect_ratio:.2f}, {diameter:.2f}, {volume:.2f}")
-# print the approximate tire volume
-#print(f"The approximate volume is {volume:.2f} liters")
+
 
