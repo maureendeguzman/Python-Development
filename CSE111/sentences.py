7478ea9138d2f8b710bb5dd995d6543b88ded13b
maureendeguzman <yhumi30@gmail.com>
2023-05-14 18:04:35 +0800

    add the home page for wdd130

--- CSE111/sentences.py
+++ CSE111/sentences.py
@@ -1,107 +1,31 @@
 import random
 def main():
-    """Return a randomly chosen determiner. A determiner is
-    a word like "the", "a", "one", "some", "many".
-    If quantity is 1, this function will return either "a",
-    "one", or "the". Otherwise this function will return
-    either "some", "many", or "the".
-
-    Parameter
-        quantity: an integer.
-            If quantity is 1, this function will return a
-            determiner for a single noun. Otherwise this
-            function will return a determiner for a plural
-            noun.
-    Return: a randomly chosen determiner.
-    """
-    quantity = 1
-    if quantity == 1:
-        words = ["a", "one", "the"]
-    else:
-        words = ["some", "many", "the"]
-
-    # Randomly choose and return a determiner.
-    determiner= (words, words)
-    cap_word = determiner.capitalize(:0)
-    return determiner
-
-def get_noun(quantity):
-    """Return a randomly chosen noun.
-    If quantity is 1, this function will
-    return one of these ten single nouns:
-        "bird", "boy", "car", "cat", "child",
-        "dog", "girl", "man", "rabbit", "woman"
-    Otherwise, this function will return one of
-    these ten plural nouns:
-        "birds", "boys", "cars", "cats", "children",
-        "dogs", "girls", "men", "rabbits", "women"
-
-    Parameter
-        quantity: an integer that determines if
-            the returned noun is single or plural.
-    Return: a randomly chosen noun.
-    """
-    if quantity == 1:
-        get_noun = ["bird", "boy", "car", "cat", "child",
-        "dog", "girl", "man", "rabbit", "woman"]
-    else:
-        prural = ["birds", "boys", "cars", "cats", "children",
-        "dogs", "girls", "men", "rabbits", "women"]
-        
-    word1 = random.choice(get_noun, prural)
-    return word1
-
-def get_verb(quantity, tense):
-    """Return a randomly chosen verb. If tense is "past",
-    this function will return one of these ten verbs:
-        "drank", "ate", "grew", "laughed", "thought",
-        "ran", "slept", "talked", "walked", "wrote"
-    If tense is "present" and quantity is 1, this
-    function will return one of these ten verbs:
-        "drinks", "eats", "grows", "laughs", "thinks",
-        "runs", "sleeps", "talks", "walks", "writes"
-    If tense is "present" and quantity is NOT 1,
-    this function will return one of these ten verbs:
-        "drink", "eat", "grow", "laugh", "think",
-        "run", "sleep", "talk", "walk", "write"
-    If tense is "future", this function will return one of
-    these ten verbs:
-        "will drink", "will eat", "will grow", "will laugh",
-        "will think", "will run", "will sleep", "will talk",
-        "will walk", "will write"
-
-    Parameters
-        quantity: an integer that determines if the
-            returned verb is single or plural.
-        tense: a string that determines the verb conjugation,
-            either "past", "present" or "future".
-    Return: a randomly chosen verb.
-    """
-    if quantity == tense:
-        get_verb = ["drank", "ate", "grew", "laughed", "thought",
-        "ran", "slept", "talked", "walked", "wrote"]
-    elif quantity == 1:
-        present = ["drinks", "eats", "grows", "laughs", "thinks",
-        "runs", "sleeps", "talks", "walks", "writes"]
-    elif quantity != 1:
-        present_0 = [ "drink", "eat", "grow", "laugh", "think",
-        "run", "sleep", "talk", "walk", "write"]
-    else:
-        future = [ "will drink", "will eat", "will grow", "will laugh",
-        "will think", "will run", "will sleep", "will talk",
-        "will walk", "will write"]
-    verb = random.choice(get_verb, present, present_0, future)
-    get_determiner = (get_verb, present, present_0, future)
-    return verb
-
+    determiner = []
+    noun = []
+    verb = []
+    prepositional_phrase = []
+    
+    
+    
+    
+    #single	past 
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+    #single	present
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+	#single	future
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+	#plural	past
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+	#plural	present
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+	#plural	future
+    print(f"{determiner} {noun} {verb} {prepositional_phrase}")
+    
+     
+   
+    
 def make_sentence(quantity, tense):
-    """Build and return a sentence with three words:
-    a determiner, a noun, and a verb. The grammatical
-    quantity of the determiner and noun will match the
-    number in the quantity parameter. The grammatical
-    quantity and tense of the verb will match the number
-    and tense in the quantity and tense parameters.
-    """
+   
     if quantity == 1:
         determiner = "words" 
     else:
@@ -116,9 +40,65 @@ def make_sentence(quantity, tense):
                 verb = verb + "s"
             else:
                 verb = verb + "ed"
-                
-    print(f"{determiner} {noun} {verb}")
+    return determiner
+
+def get_determiner(determiner):
+ 
+    quantity = 1
+    
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["some", "many", "the"]
+
+    # Randomly choose and return a determiner.
+    determiner= (words, words)
+    cap_word = determiner.capitalize(0)
+    return determiner
+
+def get_noun(quantity):
+    if quantity == 1:
+        get_noun = ["bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        prural = ["birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"]
         
+    word1 = random.choice(get_noun, prural)
+    return word1
+
+def get_verb(quantity, tense):
+    if quantity == tense:
+        get_verb = ["drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"]
+    elif quantity == 1:
+        present_p = ["drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"]
+    elif quantity != 1:
+        present_s = [ "drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"]
+    else:
+        future = [ "will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"]
+    verb = random.choice(get_verb, present_p, present_s, future)
+    get_determiner = (get_verb, present_p, present_s, future)
+    return verb
+def get_preposition():
+    prepositions = ["about", "above", "across", "after", "along",
+                    "around", "at", "before", "behind", "below",
+                    "beyond", "by", "despite", "except", "for",
+                    "from", "in", "into", "near", "of",
+                    "off", "on", "onto", "out", "over",
+                    "past", "to", "under", "with", "without"]
+    return random.choice(prepositions)
+
+def get_prepositional_phrase(quantity):
+    preposition = get_preposition()
+    determiner = get_determiner(quantity)
+    noun = get_noun(quantity)
+    prepositional_phrase = f"{preposition} {determiner} {noun}"
+    return prepositional_phrase        
 # Call the main function so that
 # this program will start executing.
 main()    
