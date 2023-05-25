366182b0c143d8f8bdec406a7cdcce0b1fcf211c
maureendeguzman <yhumi30@gmail.com>
2023-05-25 23:29:57 +0800

    added the home page for wdd130

--- CSE111/1week5TeamActivity/test_name.py
+++ CSE111/1week5TeamActivity/test_name.py
@@ -1,30 +1,31 @@
-from names import make_full_name, \
-    extract_family_name, extract_given_name
+from names import make_full_name, extract_family_name, extract_given_name
 import pytest
 
+#Core Requirements
+'''rite a function named test_make_full_name that takes no parameters. 
+Write assert statements inside this function to test the value returned from the make_full_name function. '''
 def test_make_full_name():
-    given_name = "Sally"
-    family_name = "Brown"
-    expected_result = "Brown, Sally" 
-    
-    result = make_full_name(given_name, family_name)
-    assert result == expected_result
-    
-    
-    
+    assert make_full_name("Sally", "Brown") == "Brown; Sally"  
+    assert make_full_name("James", "Madison") == "Madison; James"
+    assert make_full_name("J", "Ng") == "Ng; J"
+    assert make_full_name("", "") == "; "
+    assert make_full_name("Sally G.", "Brown") == "Brown; Sally G."  
+
 def test_extract_family_name():
-    full_name = ""
-    expected_result = ""
-
-    result = extract_family_name(full_name)
-    assert result == expected_result
-
+    assert extract_family_name("Brown; Sally") == "Brown"  
+    assert extract_family_name("Madison; James") == "Madison"
+    assert extract_family_name("Ng; J") == "Ng"
+    assert extract_family_name("; ") == ""
+    assert extract_family_name("Brown; Sally G.") == "Brown"  
+        
 def test_extract_given_name():
-    full_name = ""
-    expected_result = ""
-
-    result = extract_given_name(full_name)
-
-    assert result == expected_result  
+    assert extract_given_name("Brown; Sally") == "Sally"      
+    assert extract_given_name("Madison; James") == "James"
+    assert extract_given_name("Ng; J") == "J"
+    assert extract_given_name("; ") == ""
+    assert extract_given_name("Brown; Sally G.") == "Sally G." 
     
-pytest.main(["-v", "--tb=line", "-rN", __file__])      
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
